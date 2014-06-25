35a3eec255ef792d2ef4c802ed794008881364f7
svmatthews <matthews.sam@gmail.com>
2014-06-24 19:31:03 -0700

    working on some stuffz

--- js/site.js
+++ js/site.js
@@ -2,7 +2,7 @@ function init() {
 	// meeting maps
 	geojson['features'].push(newFeature);
 
-	var map = L.mapbox.map('map', 'examples.map-9ijuk24y')
+	var map = L.mapbox.map('map', 'examples.map-i86nkdio')
     	.setView([lat, lng], 13)
     	.featureLayer.setGeoJSON(geojson);
 }
