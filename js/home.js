55bbc9c83c76c98a27f2b889391eccc539138eae
svmatthews <matthews.sam@gmail.com>
2014-06-26 19:03:23 -0700

    adding some map libraries and a couple styles

---
+++ js/home.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  var map = L.mapbox.map('hero-map', 'examples.map-i86nkdio')
+    .setView([40, -74.50], 9);
+});
+
--- js/site.js
+++ js/site.js
@@ -1,10 +1,10 @@
 function init() {
 	// meeting maps
-	geojson['features'].push(newFeature);
+	// geojson['features'].push(newFeature);
 
-	var map = L.mapbox.map('map', 'examples.map-i86nkdio')
-    	.setView([lat, lng], 13)
-    	.featureLayer.setGeoJSON(geojson);
+	// var map = L.mapbox.map('map', 'examples.map-i86nkdio')
+ //    	.setView([lat, lng], 13)
+ //    	.featureLayer.setGeoJSON(geojson);
 }
 
 window.onLoad = init();
