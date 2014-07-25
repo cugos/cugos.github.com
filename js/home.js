cb10a293e97600f253a2d40c3047cf0f4fda125a
svmatthews <matthews.sam@gmail.com>
2014-07-24 21:08:44 -0700

    working on homepage

--- js/home.js
+++ js/home.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
-  var map = L.mapbox.map('hero-map', 'examples.map-i86nkdio')
-    .setView([47.6097, -122.3331], 11)
+  var map = L.mapbox.map('hero-map', 'examples.map-i86nkdio', {
+    zoomControl: false
+  }).setView([47.6097, -122.3331], 11)
     .featureLayer.setGeoJSON(cugos);
+
+  map.dragging.disable();
+  map.touchZoom.disable();
+  map.doubleClickZoom.disable();
+  map.scrollWheelZoom.disable();
 });
 
