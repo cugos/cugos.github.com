e70bdf262c7e3af7ee9ee1cc4fb0e3863f56c3ba
svmatthews <matthews.sam@gmail.com>
2014-08-15 18:37:23 -0700

    paco taco @thebigspoon

--- js/people.js
+++ js/people.js
@@ -1,5 +1,12 @@
-function init() {
-  console.log(map.getCenter());
-}
-
-window.onLoad = init();
+$(document).ready(function(){
+  var paco = '';
+  $(window).bind('keypress', function(e){
+    key = String.fromCharCode(e.keyCode || e.which);
+    paco += key;
+    check = paco.substr(paco.length - 4);
+    if(check=='paco') {
+      console.log($('#thebigspoon img').attr('src'));
+      $('#thebigspoon img').attr('src', '/image/paco.png');
+    }
+  });
+});
