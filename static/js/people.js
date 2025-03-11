93475fbf0eb5bc6b0eab010dd2bf79112fb95bcb
Nathaniel Henry <nat.henry7@gmail.com>
2025-03-10 23:24:33 -0700

    Fix interactive map of CUGOSians.

--- static/js/people.js
+++ static/js/people.js
@@ -10,10 +10,32 @@ $(document).ready(function(){
     }
   });
 
-  L.mapbox.accessToken = 'pk.eyJ1IjoiY3Vnb3MiLCJhIjoidGNnSlBNTSJ9.qPHDxAemDindkSskKNv90g';
-  var map = L.mapbox.map('people-map', 'cugos.jphcb71d')
+  var map = L.map('people-map')
     .setView([40, -74.50], 9);
-  var fl = L.mapbox.featureLayer();
-  fl.setGeoJSON(peopleGeoJson).addTo(map);
-  map.fitBounds(fl.getBounds());
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+  var cugosIcon = L.icon({
+    iconUrl: '/image/logos/cugos-sticker-round.svg',
+    iconSize: [25, 25],
+    shadowUrl: '/image/logos/logo-shadow-50px.png',
+    shadowSize: [25, 25],
+    shadowAnchor: [11, 11]
+  });
+  var person_layer = L
+    .geoJSON(
+      peopleGeoJson,
+      {
+        pointToLayer: function(_, latlng){
+          return L.marker(latlng, {icon: cugosIcon, opacity: 0.9})
+        }
+      }
+    )
+    .bindTooltip(
+      function (layer) { return layer.feature.properties.title},
+      {direction: 'top'}
+    );
+  person_layer.addTo(map);
+  map.fitBounds(person_layer.getBounds());
 });
